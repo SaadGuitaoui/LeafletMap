8c9720db0fe2ef10ba5e030d2e0c9e68fc06c67a
SaadGuitaoui <guitaouisaad@gmail.com>
2024-03-07 09:19:39 +0100

    test geo

--- geo/points.js
+++ geo/points.js
@@ -1,1 +1,5 @@
 var pointsJson = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"Name":"Emsi Maarif"},"geometry":{"coordinates":[-7.642424700714429,33.58370738641932],"type":"Point"},"id":0},{"type":"Feature","properties":{"Name":"Emsi les oranges"},"geometry":{"coordinates":[-7.673253728837324,33.54181024036208],"type":"Point"},"id":1},{"type":"Feature","properties":{"Name":"Emsi Rabat"},"geometry":{"coordinates":[-6.82904172883525,34.01632901804999],"type":"Point"},"id":2},{"type":"Feature","properties":{"Name":"Emsi Marrakech"},"geometry":{"coordinates":[-8.012718157640279,31.631234511497127],"type":"Point"},"id":3}]}
+
+
+
+
